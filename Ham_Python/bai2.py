7a31be4aa9c0e6086f0970628ffc1f6cbbb9d6ed
Phan Phu Tuan <phutuan112@gmail.com>
2023-12-06 00:16:19 +0700

    kiem tra so nguyen to

---
+++ Ham_Python/bai2.py
@@ -0,0 +1,9 @@
+def kiem_tra_so_nguyen_to(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(kiem_tra_so_nguyen_to(7))
